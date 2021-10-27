f3882cb48d57420233d21fccefee1a932818f5ca
Davi F <davi.felix.lima@gmail.com>
2021-10-26 23:08:15 -0300

    feature: CreateRoom page styled

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Home } from "./pages/Home";
 
 import './global.scss';
+import { CreateRoom } from "./pages/CreateRoom";
 
 function App() {
   return (
-    <Home />
+    <CreateRoom />
   );
 }
 
---
+++ src/pages/CreateRoom/index.tsx
@@ -0,0 +1,34 @@
+import illustrationImg from '../../assets/img/illustration.svg';
+import logoImg from '../../assets/img/logo.svg';
+
+import './styles.scss'
+
+import { MainButton } from "../../components/MainButton";
+
+export function CreateRoom() {
+  return (
+    <div id="page-auth">
+      <aside>
+        <img src={illustrationImg} alt="Ilustração para perguntas e respostas" />
+        <strong>Crie salas de Q&amp;A ao vivo</strong>
+        <p>Tire as dúvidas de sua audiência em tempo real</p>
+      </aside>
+      <main>
+        <div className="main-content">
+          <img src={logoImg} alt="Letmeask" />
+          <h2>Criar uma nova sala</h2>
+          <form>
+            <input 
+              type="text" 
+              placeholder="Nome da sala"
+            />
+            <MainButton type="submit">
+              Criar sala
+            </MainButton>
+          </form>
+          <p>Quer entrar em uma sala existente? <a href="#">Clique aqui</a></p>
+      </div>
+        </main>
+    </div>
+  );
+}
